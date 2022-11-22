d8f8da01ad07b6566aab1d66166fe8a72ae58cac
man_say <mansayanayaki@gmail.com>
2022-11-22 15:07:30 +0600

    adding main menu and pop up menu to languange switch

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const popUpMenu = document.getElementById('language__menu');
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const modal = document.querySelector(button.dataset.modalTarget)
+      openModal(modal)
+    })
+  })
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.language__menu.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    })
+  })
+
+
+  function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+  }
+  
+  function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+  }
